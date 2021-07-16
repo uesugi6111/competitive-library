38fd2545d9776a3c5a886fc2897a095968794539
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-07-16 03:53:02 +0000

    pass library checker

--- src/graph/lowest_common_ancestor.rs
+++ src/graph/lowest_common_ancestor.rs
@@ -18,14 +18,11 @@ pub struct LowestCommonAncestor {
 
 impl LowestCommonAncestor {
     pub fn new(edges: &[Vec<i64>], root: usize) -> Self {
-        // 変数の用意
-
         let max_v = edges.len();
         let max_log_v = ((max_v as f64).ln() / 2.0_f64.ln()) as usize + 1;
         let mut ancestors = vec![vec![None; max_v]; max_log_v + 1];
         let mut depth = vec![0; max_v];
 
-        // parent[0]とdepthを初期化する
         let mut q = std::collections::VecDeque::new();
         q.push_back(Node::new(None, root, 0));
         while let Some(u) = q.pop_front() {
@@ -41,10 +38,9 @@ impl LowestCommonAncestor {
 
         (0..max_log_v).for_each(|i| {
             (0..max_v).for_each(|j| {
-                ancestors[i + 1][j] = match ancestors[i][j] {
-                    Some(ancetor) => ancestors[i][ancetor],
-                    None => None,
-                };
+                if let Some(ancetor) = ancestors[i][j] {
+                    ancestors[i + 1][j] = ancestors[i][ancetor];
+                }
             })
         });
 
@@ -55,33 +51,33 @@ impl LowestCommonAncestor {
         }
     }
 
-    pub fn get_lca(&self, u: usize, v: usize) -> usize {
-        let (mut u, mut v) = (u, v);
-        // uとvの深さが同じになるまで親を辿る
-        if self.depth[u] > self.depth[v] {
-            std::mem::swap(&mut u, &mut v);
-        }
-        // depth[v]-depth[u]>=2^kとなる最小のkを求める。
-        // つまりuをvと深さが同じか小さいぎりぎりのところまで親を辿る。
+    pub fn get_lca(&self, u: usize, v: usize) -> Option<usize> {
+        let (mut u, mut v) = if self.depth[u] > self.depth[v] {
+            (v, u)
+        } else {
+            (u, v)
+        };
+
         for k in 0..self.max_log_v {
             if (((self.depth[v] - self.depth[u]) >> k) & 1) == 1 {
                 v = self.ancestors[k][v].unwrap();
             }
         }
+
         if u == v {
-            return u;
+            return Some(u);
         }
-        // uとvが衝突しないように辿る。
+
         for k in (0..self.max_log_v).rev() {
-            if self.ancestors[k][u] != self.ancestors[k][v]
-                && self.ancestors[k][u].is_none()
-                && self.ancestors[k][v].is_none()
+            if self.ancestors[k][u].is_some()
+                && self.ancestors[k][v].is_some()
+                && self.ancestors[k][u] != self.ancestors[k][v]
             {
                 u = self.ancestors[k][u].unwrap();
                 v = self.ancestors[k][v].unwrap();
             }
         }
-        self.ancestors[0][u].unwrap()
+        self.ancestors[0][u]
     }
 
     pub fn get_distance() -> i64 {
@@ -103,7 +99,7 @@ mod tests {
 
         let lca = LowestCommonAncestor::new(&e, 0);
         for &(u, v, ans) in [(0, 1, 0), (0, 4, 0), (1, 2, 0), (2, 3, 2), (3, 4, 2)].iter() {
-            assert_eq!(lca.get_lca(u, v), ans);
+            assert_eq!(lca.get_lca(u, v).unwrap_or(0), ans);
         }
     }
 }
