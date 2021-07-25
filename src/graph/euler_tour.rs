0f35ad2f7f31f21fc5e5897b3c501e27095df770
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-07-25 20:18:42 +0000

    fix return

--- src/graph/euler_tour.rs
+++ src/graph/euler_tour.rs
@@ -13,22 +13,36 @@ impl Vertex {
 }
 use std::collections::VecDeque;
 
-pub fn euler_tour(e: &[Vec<usize>], root: usize) -> Vec<usize> {
+pub fn euler_tour(e: &[Vec<usize>], root: usize) -> (Vec<usize>, Vec<usize>, Vec<usize>) {
     let mut stack = VecDeque::new();
-    stack.push_back(Vertex::Out(root));
     stack.push_back(Vertex::In(root));
-    let mut v = vec![];
+    let mut tour = vec![];
+    let mut first_look = vec![None; e.len()];
+    let mut depth = 0;
+    let mut depths = vec![0; e.len()];
     while let Some(vertex) = stack.pop_back() {
         if let Vertex::In(v) = vertex {
             for &to in e[v].iter() {
-                stack.push_back(Vertex::Out(to));
+                if first_look[to].is_some() {
+                    continue;
+                }
+                stack.push_back(Vertex::Out(v));
                 stack.push_back(Vertex::In(to));
             }
+            first_look[v] = Some(tour.len());
+            depths[v] = depth;
+            depth += 1;
+        } else {
+            depth -= 1;
         }
-        v.push(vertex.get_value());
+        tour.push(vertex.get_value());
     }
 
-    v
+    (
+        tour,
+        first_look.iter().map(|x| x.unwrap()).collect(),
+        depths,
+    )
 }
 
 #[cfg(test)]
@@ -38,7 +52,7 @@ mod tests {
     #[test]
     fn test_eiler_tour() {
         let e = vec![vec![5, 1], vec![4, 2], vec![3], vec![], vec![], vec![]];
-        let ans = euler_tour(&e, 0);
-        dbg!(&ans);
+        let (ans, _, _) = euler_tour(&e, 0);
+        assert_eq!(&ans, &[0, 1, 2, 3, 2, 1, 4, 1, 0, 5, 0]);
     }
 }
