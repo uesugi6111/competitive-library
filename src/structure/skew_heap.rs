12d7b7fc8630c8e9d8abad69e3ef7b2facf86fd7
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-08-08 11:58:49 +0000

    refactor

--- src/structure/skew_heap.rs
+++ src/structure/skew_heap.rs
@@ -17,47 +17,54 @@ impl<T: Ord + Clone> Heap<T> {
         }))
     }
 }
-
-#[inline]
-pub fn merge<T: Ord + Clone>(a: &mut Option<Box<Heap<T>>>, mut b: Option<Box<Heap<T>>>) {
-    if a.is_none() {
-        swap(a, &mut b);
-        return;
-    }
-    if b.is_none() {
-        return;
-    }
-    if a > &mut b {
-        swap(a, &mut b);
-    }
-    merge(&mut a.as_mut().unwrap().right, b);
-
-    let tmp = a.as_mut().unwrap();
-    swap(&mut tmp.left, &mut tmp.right);
+#[derive(Default, Clone)]
+pub struct SkewHeap<T: Ord + Clone> {
+    node: Option<Box<Heap<T>>>,
 }
+impl<T: Ord + Clone> SkewHeap<T> {
+    pub fn new() -> Self {
+        Self { node: None }
+    }
 
-#[inline]
-pub fn push<T: Ord + Clone>(heap: &mut Option<Box<Heap<T>>>, value: T) {
-    merge(heap, Heap::new(value));
-}
+    #[inline]
+    pub fn push(&mut self, value: T) {
+        SkewHeap::merge(&mut self.node, Heap::new(value));
+    }
+    #[inline]
+    pub fn top(&self) -> Option<T> {
+        Some(self.node.as_ref()?.value.clone())
+    }
+    #[inline]
+    pub fn pop(&mut self) -> Option<T> {
+        let value = self.top()?;
 
-#[inline]
-pub fn top<T: Ord + Clone>(heap: &Option<Box<Heap<T>>>) -> Option<T> {
-    Some(heap.as_ref()?.value.clone())
-}
+        let (mut left, right) = {
+            let mut tmp = self.node.take().unwrap();
+            (tmp.left.take(), tmp.right.take())
+        };
+        SkewHeap::merge(&mut left, right);
+        swap(&mut self.node, &mut left);
 
-#[inline]
-pub fn pop<T: Ord + Clone>(heap: &mut Option<Box<Heap<T>>>) -> Option<T> {
-    let value = top(heap)?;
+        Some(value)
+    }
 
-    let (mut left, right) = {
-        let mut tmp = heap.take().unwrap();
-        (tmp.left.take(), tmp.right.take())
-    };
-    merge(&mut left, right);
-    swap(heap, &mut left);
+    #[inline]
+    pub fn merge(a: &mut Option<Box<Heap<T>>>, mut b: Option<Box<Heap<T>>>) {
+        if a.is_none() {
+            swap(a, &mut b);
+            return;
+        }
+        if b.is_none() {
+            return;
+        }
+        if a > &mut b {
+            swap(a, &mut b);
+        }
+        SkewHeap::merge(&mut a.as_mut().unwrap().right, b);
 
-    Some(value)
+        let tmp = a.as_mut().unwrap();
+        swap(&mut tmp.left, &mut tmp.right);
+    }
 }
 
 #[cfg(test)]
@@ -65,23 +72,23 @@ mod tests {
     use super::*;
     #[test]
     fn test_heap() {
-        let mut a = vec![None; 5];
+        let mut a = vec![SkewHeap::new(); 5];
 
         for i in 0..30 {
-            push(&mut a[i % 5], i);
+            a[i % 5].push(i);
         }
 
         for (i, e) in a.iter().enumerate() {
-            assert_eq!(top(e).unwrap(), i);
+            assert_eq!(e.top().unwrap(), i);
         }
 
         for i in 1..5 {
-            let buff = a[i].take();
-            merge(&mut a[0], buff);
+            let buff = a[i].node.take();
+            SkewHeap::merge(&mut a[0].node, buff);
         }
 
         for i in 0..30 {
-            assert_eq!(pop(&mut a[0]).unwrap(), i);
+            assert_eq!(a[0].pop().unwrap(), i);
         }
     }
 }
