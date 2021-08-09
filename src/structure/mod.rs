32cf5cc6a85e45fad2636a9baef97a617547d61e
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-08-09 08:20:12 +0000

    rename

--- src/structure/mod.rs
+++ src/structure/mod.rs
@@ -1,8 +1,8 @@
 //! データ構造っぽいもの
 pub mod binary_trie;
 pub mod cumsum_2d;
+pub mod disjoint_set_union;
 pub mod disjoint_sparse_table;
-pub mod dsu;
 pub mod fenwick_tree;
 pub mod segment_tree;
 pub mod skew_heap;
