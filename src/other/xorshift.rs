d7898a7ea92a29582d47fbc9e87a6d0f87d32948
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-09-07 15:37:52 +0000

    impl xorshift

---
+++ src/other/xorshift.rs
@@ -0,0 +1,44 @@
+use std::time::SystemTime;
+
+#[derive(Debug, Default)]
+pub struct XorShift {
+    seed: u64,
+}
+
+impl XorShift {
+    pub fn new() -> Self {
+        Self {
+            seed: SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs(),
+        }
+    }
+}
+
+impl Iterator for XorShift {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.seed ^= self.seed << 13;
+        self.seed ^= self.seed >> 7;
+        self.seed ^= self.seed << 17;
+        Some(self.seed)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashSet;
+    #[test]
+    fn test_xorshift() {
+        let mut set = HashSet::new();
+        let xorshift = XorShift::new();
+
+        for v in xorshift.take(100_000) {
+            assert!(!set.contains(&v));
+            set.insert(v);
+        }
+    }
+}
