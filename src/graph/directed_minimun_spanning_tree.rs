98d3ddea6d9dcce7f1194c7a41a1cc7ed5a43447
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-09-08 15:58:02 +0000

    impl

--- src/graph/directed_minimun_spanning_tree.rs
+++ src/graph/directed_minimun_spanning_tree.rs
@@ -3,8 +3,6 @@ use std::cmp::Ordering;
 use crate::structure::disjoint_set_union::Dsu;
 use crate::structure::skew_heap_lazy::SkewHeap;
 
-use crate::structure::disjoint_set_union_undo::DisjointSetUnionRollback;
-
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd)]
 struct Edge {
     from: usize,
@@ -17,8 +15,14 @@ impl Ord for Edge {
         self.cost.cmp(&(other.cost))
     }
 }
-pub fn directed_mst(e: &[Vec<(usize, i64)>], root: usize) {
+pub fn directed_mst(e: &[Vec<(usize, i64)>], root: usize) -> i64 {
+    let mut uf = Dsu::new(e.len());
+
+    let mut from_v = vec![0; e.len()];
+    let mut from_cost = vec![0; e.len()];
+    let mut used = vec![0; e.len()];
     let mut heap = vec![SkewHeap::new(); e.len()];
+    used[root] = 2;
 
     for i in 0..e.len() {
         for j in 0..e[i].len() {
@@ -32,4 +36,64 @@ pub fn directed_mst(e: &[Vec<(usize, i64)>], root: usize) {
             );
         }
     }
+
+    let mut ans = 0;
+    for start in 0..e.len() {
+        if used[start] != 0 {
+            continue;
+        }
+        let mut cur = start;
+        let mut processing = vec![];
+        while used[cur] != 2 {
+            used[cur] = 1;
+            processing.push(cur);
+
+            if heap[cur].is_empty() {
+                return 0;
+            }
+            if let Some((c, Edge { from, to, cost })) = heap[cur].pop() {
+                from_v[cur] = uf.root(from);
+                from_cost[cur] = c;
+            }
+            if from_v[cur] == cur {
+                continue;
+            }
+            ans += from_cost[cur];
+
+            if used[from_v[cur]] == 1 {
+                let mut p = cur;
+                if !heap[p].is_empty() {
+                    heap[p].add(-from_cost[p])
+                };
+                if p != cur {
+                    uf.unite(p, cur);
+                    SkewHeap::merge(&mut heap[cur].node, heap[p].node);
+                }
+                p = uf.root(from_v[p]);
+                while p != cur {
+                    if !heap[p].is_empty() {
+                        heap[p].add(-from_cost[p])
+                    };
+                    if p != cur {
+                        uf.unite(p, cur);
+                        SkewHeap::merge(&mut heap[cur].node, heap[p].node);
+                    }
+                    p = uf.root(from_v[p]);
+                }
+            } else {
+                cur = from_v[cur];
+            }
+        }
+        for v in processing {
+            used[v] = 2;
+        }
+    }
+    ans
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_directed_mst() {}
 }
