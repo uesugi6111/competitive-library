7b62384eae7b9561fd6301009501cc88a7c181c4
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-09-15 23:52:31 +0000

    dekita

--- src/math/miller_rabin.rs
+++ src/math/miller_rabin.rs
@@ -2,7 +2,7 @@ use crate::math::mod_pow::modpow;
 use crate::other::xorshift::XorShift;
 
 pub fn is_prime(n: i64) -> bool {
-    if n == 2 {
+    if n == 2 || n == 3 {
         return true;
     } else if n == 1 || (n > 2 && n & 1 == 0) {
         return false;
@@ -15,14 +15,18 @@ pub fn is_prime(n: i64) -> bool {
         t >>= 1;
     }
 
-    let a = XorShift::new().next().unwrap() as i64 % n;
+    let r = {
+        let mut r = XorShift::from_seed(1).next().unwrap() as i64 % (n - 3);
+        r += 3;
+        r
+    };
 
-    if modpow(a, t, n as i64) == 1 {
+    if modpow(r, t, n as i64) == 1 {
         return true;
     }
 
     for i in 0..s {
-        if modpow(a, 2_i64.pow(i) * t, n as i64) == n - 1 {
+        if modpow(r, 2_i64.pow(i) * t, n as i64) == n - 1 {
             return true;
         }
     }
@@ -49,9 +53,24 @@ mod tests {
             (10, false),
             (11, true),
             (1_000_000_007, true),
+            (8191, true),
+            (131_071, true),
+            (524_287, true),
+            (6_700_417, true),
+            (2_147_483_647, true),
+            (67_280_421_310_721, true),
+            (67_280_421_310_722, false),
+            (100_000_000_000_000, false),
+            (68_718_297_093, false), // 131071 * 524287
         ];
         for (i, ans) in v {
             assert_eq!(is_prime(i), ans, "testing {}", i);
+        }
+    }
+    #[test]
+    fn test_miller_rabin_loop() {
+        for _ in 0..10 {
+            test_miller_rabin();
         }
     }
 }
