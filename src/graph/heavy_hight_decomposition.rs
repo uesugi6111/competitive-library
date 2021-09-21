5acd4c9d757a239813ff655367c8aedd2b42a606
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-09-21 15:09:29 +0000

    impl depth

--- src/graph/heavy_hight_decomposition.rs
+++ src/graph/heavy_hight_decomposition.rs
@@ -2,14 +2,19 @@
 
 pub struct HeavyLightDecomposition {
     s: Vec<usize>,
+    d: Vec<usize>,
     c: Vec<Vec<usize>>,
+
+    parent: Vec<usize>,
 }
 
 impl HeavyLightDecomposition {
-    pub fn new(e: &[Vec<usize>]) -> Self {
+    pub fn new(parent: &[usize]) -> Self {
         Self {
-            s: vec![0; e.len()],
+            s: vec![0; parent.len()],
             c: vec![],
+            d: vec![0; parent.len()],
+            parent: parent.to_vec(),
         }
     }
     pub fn count_node(&mut self, v: usize) -> usize {
@@ -21,5 +26,16 @@ impl HeavyLightDecomposition {
             self.s[v] += self.count_node(self.c[v][i]);
         }
         self.s[v]
+    }
+
+    pub fn depth(&mut self, v: usize) -> usize {
+        if self.d[v] != 0 {
+            return self.d[v];
+        }
+        if self.parent[v] == v {
+            return 0;
+        }
+        self.d[v] = self.depth(self.parent[v]) + 1;
+        self.d[v]
     }
 }
