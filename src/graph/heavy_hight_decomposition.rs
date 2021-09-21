4c0749dd4c849e631c14af1b6b6c0b877756f64b
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-09-21 17:49:59 +0000

    a lot of bugs

--- src/graph/heavy_hight_decomposition.rs
+++ src/graph/heavy_hight_decomposition.rs
@@ -13,7 +13,7 @@ pub struct HeavyLightDecomposition {
 impl HeavyLightDecomposition {
     pub fn new(parent: &[usize]) -> Self {
         let e = {
-            let mut e = vec![vec![]];
+            let mut e = vec![vec![]; parent.len()];
             for (i, &v) in parent.iter().enumerate() {
                 e[v].push(i);
             }
@@ -82,5 +82,41 @@ impl HeavyLightDecomposition {
         }
         self.depths[v] = self.depth(self.parent[v]) + 1;
         self.depths[v]
+    }
+
+    pub fn query(&mut self, u: usize, v: usize) -> Vec<(usize, usize)> {
+        let (mut u, mut v) = (u, v);
+
+        let mut ret = vec![];
+        while self.head[u] != self.head[v] {
+            if self.depth(self.head[u]) <= self.depth(self.head[v]) {
+                ret.push((self.pre[self.head[v]], self.pre[v]));
+                v = self.parent[self.head[v]];
+            } else {
+                ret.push((self.pre[self.head[u]], self.pre[u]));
+                u = self.parent[self.head[u]];
+            }
+        }
+        ret.push(if self.pre[u] < self.pre[v] {
+            (self.pre[u], self.pre[v])
+        } else {
+            (self.pre[v], self.pre[u])
+        });
+        ret
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_hld() {
+        let v = vec![0, 0, 1, 2, 2, 1, 0, 6, 7, 7, 0, 10];
+
+        let mut hld = HeavyLightDecomposition::new(&v);
+        hld.decompose();
+
+        let a = hld.query(4, 9);
+        dbg!(&a);
     }
 }
